23e3db54852e9a46c927b537396e9b478495d5a8
ehitco <aiden@ehnote.cn>
2023-02-11 17:27:48 +0800

    feat: typeorm mysql user
    新增mysql 数据库连接user实体

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { file as envFilePath } from '@ehitco/config';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: [envFilePath] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST', '0.0.0.0'),
+        port: configService.get<number>('DB_PORT', 3306),
+        username: configService.get('DB_USER', 'root'),
+        password: configService.get('DB_PASSWD', 'root'),
+        database: configService.get('DB_DATABASE', 'ehitco'),
+        charset: 'utf8mb4',
+        timezone: '+08:00',
+        autoLoadEntities: true, // 如果为true,将自动加载实体 forFeature()方法注册的每个实体都将自动添加到配置对象的实体数组中
+        synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+      }),
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
